cacbe939d9fcb147039d8cbb487520ec7a38fe08
xiongzq <xzq17630061068@gmail.com>
2025-08-16 15:58:16 +0800

    init

--- contracts/xzq_5_ArrayExample.sol
+++ contracts/xzq_5_ArrayExample.sol
@@ -5,5 +5,19 @@ pragma solidity ^0.8.0;
 contract ArrayExample{
     uint256[] public numbers;
 
-    
+    function addNumber(uint256 _num) public {
+        numbers.push(_num);
+    }
+
+    function removeLast() public{
+        numbers.pop();
+    }
+
+    function getNumber(uint256 index)public view returns (uint256){
+        return numbers[index];
+    }
+
+    function length() public view returns (uint256){
+        return numbers.length;
+    }
 }
